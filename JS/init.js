8ee97f2902e9204b9877f52eb78ab981897f703f
Tom_Laptop <tom.gebhardt@uni-rostock.de>
2017-09-19 00:36:05 +0200

    read files, display xml or send to bives?

--- JS/init.js
+++ JS/init.js
@@ -1,12 +1,10 @@
 function init(){
-  $("#compareButton").click(function(){alert("compare!")});
-
+  //add function to compare button
+  $("#compareButton").click(function(){compareAllFiles()});
   //add event listener to file listener
   document.getElementById('files').addEventListener('change', handleFilesSelect, false);
   document.getElementById('orgFile').addEventListener('change', handleFileSelect, false);
-
 }
-
 
 function handleFilesSelect(evt) {
   var files = evt.target.files; // FileList object
@@ -22,7 +20,7 @@ function handleFilesSelect(evt) {
                 '</li>'
               );
   }
-  document.getElementById('orgFileList').innerHTML = '<ul class="w3-ul w3-card-4">' + output.join('') + '</ul>';
+  document.getElementById('list').innerHTML = '<ul class="w3-ul w3-card-4">' + output.join('') + '</ul>';
 }
 
 function handleFileSelect(evt) {
@@ -39,5 +37,56 @@ function handleFileSelect(evt) {
                 '</li>'
               );
   }
-  document.getElementById('list').innerHTML = '<ul class="w3-ul w3-card-4">' + output.join('') + '</ul>';
+  document.getElementById('orgFileList').innerHTML = '<ul class="w3-ul w3-card-4">' + output.join('') + '</ul>';
+}
+
+function compareAllFiles(orgFile, altFile){
+  for(i=0; i<document.getElementById("files").files.length; i++){
+    //create DOM elements
+
+    //call bives with orgFile and altFile
+
+        var altFile = document.getElementById("files").files[i];
+
+        //console.log(altFile);
+
+        //You could insert a check here to ensure proper file type
+        var reader = new FileReader();
+        reader.readAsText(altFile);
+        reader.onloadend = function(){
+            var xmlData = $(reader.result);
+            console.log(xmlData);
+        };
+
+  }
+}
+
+//Info-Content
+function getBivesData(v1, v2, command){
+	var data;
+	//console.log(v1.url, v2.url);
+
+	var bivesJob = {
+		files:
+		[
+			v1,
+			v2
+		],
+		commands:
+			command
+	};
+
+
+	// call the bives wrapper script
+	$.post (
+		"bives/bives.php",
+		"bivesJob=" + JSON.stringify (bivesJob),
+		function (data)
+		{
+			//obj = showSbgn(JSON.parse(data).reactionsSbgnJson);
+			console.log(obj);
+			$("#bivesReport").html ($.parseJSON (data).reportHtml);
+			var sbgnJson = $.parseJSON (data).reactionsSbgnJson;
+		}
+	);
 }
